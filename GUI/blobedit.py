81b130465b4af06970f725bebf6ff967102aeff6
Lisa Mishra <lmishra@andrew.cmu.edu>
2022-03-16 11:15:08 -0400

    update

--- GUI/blobedit.py
+++ GUI/blobedit.py
@@ -79,7 +79,8 @@ def resize_image(frame):
 
 def mainlooprun():
     window = sg.Window('Drawbuddy', LAYOUTS, background_color='#57B5EE',
-                       resizable=True)
+                       resizable=True, finalize=True)
+    window.set_min_size((500,500))
 
     cap = cv2.VideoCapture(0)
     #faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
