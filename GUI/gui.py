8bdd60d16e952f3b2a6ea80192d75101ff6f6e6c
Ronald Gonzalez <lilbertha@ronalds-air.wifi.local.cmu.edu>
2022-04-18 10:52:17 -0400

    You can now view the received svg before accepting it

--- GUI/gui.py
+++ GUI/gui.py
@@ -17,16 +17,14 @@ from queue import Queue
 from svgutils.compose import *
 from svglib.svglib import svg2rlg
 from reportlab.graphics import renderPM
-<<<<<<< HEAD
 import zlib
-=======
 import io
 from svgParser import getData
 
->>>>>>> 418e305cd39628279de9254c5faa1be44d074cd4
 
 isHost = False
 myID = ""
+myUsername = ''
 userList = dict()
 vectors = 0
 PORT = random.randint(3456, 59897)
@@ -97,7 +95,7 @@ def whiteboardLayout():
 
     code = "0" + str(PORT) if len(str(PORT)) == 4 else str(PORT)
 
-    left_col = [[sg.Text("Whiteboard ")],
+    left_col = [[sg.Text("Whiteboard"), sg.Button('Exit')],
                 [sg.Text(get_names(userList), key='-USERLIST-')],
                 [sg.Image(key="-IMAGE-")],
                 [sg.Graph(
@@ -129,10 +127,10 @@ def whiteboardLayout():
     return layout
 
 sz=(900,700)
-LAYOUTS = [[sg.Column(mkGreetLayout(), key = '-GREET-', visible = False),
+LAYOUTS = [[sg.Column(mkGreetLayout(), key = '-GREET-', visible = True),
             sg.Column(mkHostLayout(), key = '-HOST-', visible = False),
             sg.Column(mkUserLayout(), key = '-USER-', visible = False),
-            sg.Column(whiteboardLayout(), key = '-WHITEBOARD-', visible = True, background_color = 'white')]]
+            sg.Column(whiteboardLayout(), key = '-WHITEBOARD-', visible = False, background_color = 'white')]]
 
 # takes frame and returns bytes of frame compatible with cv
 def get_bytes(frame):
@@ -181,13 +179,13 @@ def mainlooprun():
     global userList
     global isHost
     global vectors
+    global myUsername
     receivedSVG = []
     window = sg.Window('Login App',LAYOUTS, size=sz, background_color='#57B5EE',
                        resizable=True, finalize=True)
     window.set_min_size(sz)
     cap = cv2.VideoCapture(0)
     iterations = 0
-    vectorize_image = False
     image_to_vectorize = None
     
     while True:
@@ -199,9 +197,9 @@ def mainlooprun():
         try:
             while(serverMsg.qsize() > 0):
                 msg = serverMsg.get(False)
-                print("received\n")
                 msg = msg.split("\t")
                 command = msg[0]
+                print("received %s\n" % command)
                 if command == 'myIDis':
                     myID = msg[1]
                     i = 2
@@ -222,22 +220,22 @@ def mainlooprun():
                     svg = svg.decode('UTF-8')
                     receivedSVG.append(svg)
                     vectors += 1
+                if command == 'exit':
+                    userList.pop(userList.index(msg[2]))
         except:
             pass
 
         if event != "__TIMEOUT__":
             print("event:", event)
-
-        code = vals['-PORTNUM-'] if not isHost else "0" + str(PORT) if len(str(PORT)) == 4 else str(PORT)
+        try:
+            code = vals['-PORTNUM-'] if not isHost else "0" + str(PORT) if len(str(PORT)) == 4 else str(PORT)
+        except:
+            pass
         ret, frame = cap.read()
         window['-IMAGE_FEED-'].update(data = get_bytes(resize_image_home_page(frame)))
         window['-USERLIST-'].update(get_names(userList))
         window['-PORT-'].update(code)
         window['-MREC-'].update("Vectors in Inbox: %s" % str(vectors))
-
-        if vectorize_image:
-            window['-VECTORIZE_IMAGE-'].update(data = get_bytes(resize_image_signup(image_to_vectorize)))
-            vectorize_image = False
 
         if event == 'Create Session':
             window['-GREET-'].update(visible = False)
@@ -254,6 +252,7 @@ def mainlooprun():
             server.connect((HOST,PORT))
             serverMsg = Queue(100)
             threading.Thread(target = handleServerMsg, args = (server, serverMsg)).start()
+            myUsername = vals['-DISNAME1-']
             msg = 'dname\t' + vals['-DISNAME1-'] + '\n'
             server.send(msg.encode())
             window['-WHITEBOARD-'].update(visible = True)
@@ -283,7 +282,6 @@ def mainlooprun():
             window['-WHITEBOARD-'].update(visible = True)
 
         if event == "Vectorize Image":
-            vectorize_image = True
             image_to_vectorize = frame
             file_name =  'frame'
             vectorize(frame, file_name)
@@ -308,12 +306,30 @@ def mainlooprun():
             window['-FILENAME-'].update(visible=True)
             window['-SUB-'].update(visible=True)
             window['-USUB-'].update(visible=True)
+            svg = receivedSVG.pop()
+            path =  os.getcwd()[:-3]
+            temp_name = str(time.time()).replace('.', '_')
+            path += 'GUI/' + temp_name + '.svg'
+            savedSVG = open(path, 'w')
+            savedSVG.write(svg)
+            drawing = svg2rlg(path)
+            renderPM.drawToFile(drawing, temp_name + ".png", fmt="PNG")
+            image = IMG.open(temp_name + ".png")
+            image.thumbnail((500, 500))
+            bio = io.BytesIO()
+            image.save(bio, format="PNG")
+            window['-VECTORIZE_IMAGE-'].update(data=bio.getvalue())
+            os.remove(temp_name + '.png')
+            os.remove(temp_name + '.svg')
+            window['-VECTORIZE_IMAGE-'].update(visible=True)
+            receivedSVG.insert(0, svg)
             
         if event == '-SUB-':
             window['-REC-'].update(visible=True)
             window['-FILENAME-'].update(visible=False)
             window['-SUB-'].update(visible=False)
             window['-USUB-'].update(visible=False)
+            window['-VECTORIZE_IMAGE-'].update(visible=False)
             fname = vals['-FILENAME-']
             svg = receivedSVG.pop()
             path =  os.getcwd()[:-3]
@@ -335,7 +351,11 @@ def mainlooprun():
             window['-SUB-'].update(visible=False)
             window['-USUB-'].update(visible=False)
 
-        if event == sg.WIN_CLOSED or event == 'Exit1':
+        if event == sg.WIN_CLOSED or event == 'Exit':
+            msg = 'exit\t%s\n' % myUsername
+            server.send(msg.encode())
+            time.sleep(1)
+            window.close()
             break
 
         window.refresh()
