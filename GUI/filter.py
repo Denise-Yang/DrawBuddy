d0e4e2ae6941f6bfcfebc2c520efaba8365d7f64
Denise-Yang <47067329+Denise-Yang@users.noreply.github.com>
2022-04-19 22:50:40 -0400

    implemented crop image

--- GUI/filter.py
+++ GUI/filter.py
@@ -13,59 +13,46 @@ def show_image(image):
     if c >= 0 : return -1
     return 0
 
-def convertToBW(image_name):
+
+def scaleRect(x,y,w,h, scale):
+    cx = x+ w/2
+    cy = y + h/2
+    newh = h*scale
+    neww = w*scale
+    return int(cx - neww/2), int(cy - newh/2), int(neww), int(newh)
+
+def resize(img, cnt, w,h):
+    
+    #Create a rectangle around contour region of interest
+    epsilon = 0.1*cv2.arcLength(cnt,True)
+    approx = cv2.approxPolyDP(cnt,epsilon,True)
+    x,y,w1,h1 = cv2.boundingRect(approx)
+    #scale the image to remove the hand
+    x1,y1,w1,h1 = scaleRect(x,y,w1,h1,.8)
+
+    cropped_im = img[y1:y1+h1, x1:x1+w1]
+    return cropped_im
+
+
+def cropImage(image_name):
     img = cv2.imread(image_name)
     h, w, c = img.shape
     contourIm = img.copy()
     grayImage = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    (thresh, blackAndWhiteImage) = cv2.threshold(grayImage, 150, 255, cv2.THRESH_BINARY)
+    (thresh, blackAndWhiteImage) = cv2.threshold(grayImage, 170, 255, cv2.THRESH_BINARY)
     #filter noise
     kernel = np.ones((10,10),np.uint8)
     result = cv2.morphologyEx(blackAndWhiteImage, cv2.MORPH_CLOSE, kernel)
     contours, hierarchy  = cv2.findContours(result, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    print("len", len(contours))
+    
+    #draw rectangle
     #contours = contours[0] if len(contours) == 2 else contours[1]
     big_contour = max(contours, key=cv2.contourArea)
 
-    # draw white contour on black background as mask
-    mask = np.zeros((h,w), dtype=np.uint8)
-    cv2.drawContours(mask, [big_contour], 0, (255,255,255), cv2.FILLED)
-
-    # invert mask so shapes are white on black background
-    mask_inv = 255 - mask
-
-    # create new (blue) background
-    bckgnd = np.full_like(img, (255,0,0))
-
-    # apply mask to image
-    image_masked = cv2.bitwise_and(img, img, mask=mask)
-
-    # apply inverse mask to background
-    bckgnd_masked = cv2.bitwise_and(bckgnd, bckgnd, mask=mask_inv)
-
-    # add together
-    result = cv2.add(image_masked, bckgnd_masked)
-
-    #cv2.imshow("GRAY", result)
+    cropped_im = resize(contourIm, big_contour, w, h)
+    #cv2.imshow("GRAY", cropped_im)
     #cv2.waitKey(0)
 
-    print("done showing image")
     return contours
-
-def removeBackground(image):
-    contours, hierarchy  = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    result = image.copy()
-    print(contours)
-
-    cv2.drawContours(result, contours, -1, (0,255,0), 10)
-
-    cv2.imshow("GRAY", result)
-
-    print("done showing image")
-    return contours
-
-def cropImage(image):
-    bwImage = convertToBW(image)
-    #removeBackground(bwImage)
 
 
